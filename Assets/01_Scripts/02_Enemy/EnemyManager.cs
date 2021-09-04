be8e3c7b75265474fd65b6d46c90404d9b6c5ee5
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-09-04 20:39:03 +0900

    Enemy Temp Save

---
+++ Assets/01_Scripts/02_Enemy/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : ObjectManager<EnemyManager, Enemy>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //AddPool("Enemy", )
+    }
+
+    public override void OnPlayEnter()
+    {
+
+    }
+    public override void OnPlayExit()
+    {
+
+    }
+
+    public override void __Initialize()
+    {
+        base.__Initialize();
+
+        M_Game.OnPlayEnter += OnPlayEnter;
+        M_Game.OnPlayExit += OnPlayExit;
+    }
+
+    public override void __Finalize()
+    {
+        base.__Finalize();
+    }
+}
