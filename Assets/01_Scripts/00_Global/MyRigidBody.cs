b17d9a6fdfb43876360234047635f942e5bc4dd5
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-09-05 18:21:48 +0900

    MyRigidbody Update

--- Assets/01_Scripts/00_Global/MyRigidBody.cs
+++ Assets/01_Scripts/00_Global/MyRigidBody.cs
@@ -8,12 +8,10 @@ public class MyRigidBody : MonoBehaviour
     // 기본 중력
     public static readonly float Gravity = -9.8f;
 
-    public float m_TempSpeed = 1f;
-
     // 질량
     [SerializeField]
     protected float m_Mass = 1f;
-    // 공기 저항
+    // 항력 계수
     [SerializeField]
     protected float m_Drag = 0f;
     // 중력 사용 여부
@@ -22,6 +20,9 @@ public class MyRigidBody : MonoBehaviour
     // 가해지는 중력
     [SerializeField]
     protected float m_Gravity = Gravity;
+    // 물리 사용 여부
+    [SerializeField]
+    protected bool m_IsKinematic = false;
 
     #region 내부 정보
     [Space(15)]
@@ -31,20 +32,14 @@ public class MyRigidBody : MonoBehaviour
     // 저항 속도
     [SerializeField, ReadOnly]
     protected float m_DragSpeed;
-    //// 종단 속도
-    //[SerializeField, ReadOnly]
-    //protected float m_TerminalSpeed;
     // 속도
     [SerializeField, ReadOnly]
     protected Vector2 m_Velocity;
     // 가속도
-    [SerializeField, ReadOnly]
     protected Vector2 m_Acceleration;
     // 힘 (작용)
-    [SerializeField, ReadOnly]
     protected Vector2 m_Force;
     // 저항 (반작용)
-    [SerializeField, ReadOnly]
     protected Vector2 m_DragForce;
     #endregion
 
@@ -55,13 +50,22 @@ public class MyRigidBody : MonoBehaviour
     #region 내부 함수
     protected Vector2 CalculateDrag(Vector2 force)
     {
+        if (force.magnitude <= 0f)
+            return Vector2.zero;
+
+        // 밀도
         const float p = 1f;
+        // 면적
         const float A = 1f;
+        // 속도의 속력
         float v = force.magnitude;
-        float Cd = m_Drag * 0.01f;
+        // 항력 계수
+        float Cd = m_Drag;
+        // 속도의 방향
         Vector2 dir = force.normalized;
 
-        Vector2 Fd = -0.5f * p * v * v * A * Cd * dir;
+        // 항력 방정식
+        Vector2 Fd = -0.105f * p * v * v * A * Cd * dir;
 
         return Fd;
     }
@@ -79,32 +83,12 @@ public class MyRigidBody : MonoBehaviour
     #region 유니티 콜백 함수
     void FixedUpdate()
     {
-        #region 테스트
-        AddForce(Vector2.right * Input.GetAxisRaw("Horizontal") * m_TempSpeed);
-        AddForce(Vector2.up * Input.GetAxisRaw("Vertical") * m_TempSpeed);
-        #endregion
-        #region 힘 적용
-        if (m_UseGravity)
+        #region 중력
+        if (m_UseGravity && !m_IsKinematic)
         {
             Vector2 gravity = Vector2.up * m_Gravity * m_Mass;
 
             AddForce(gravity);
-        }
-
-        if (m_Force.magnitude < CalculateDrag(m_Force).magnitude)
-        {
-            m_Force = Vector2.zero;
-        }
-        if (m_Velocity.magnitude < CalculateDrag(m_Velocity).magnitude)
-        {
-            m_Velocity = Vector2.zero;
-        }
-
-        if (m_Velocity.magnitude > 0)
-        {
-            m_DragForce = CalculateDrag(m_Velocity);
-
-            AddForce(m_DragForce);
         }
         #endregion
         #region 계산
@@ -112,23 +96,51 @@ public class MyRigidBody : MonoBehaviour
         m_Velocity += m_Acceleration * Time.deltaTime;
         #endregion
         #region 예외처리
-        //if (m_Velocity.magnitude > m_TerminalSpeed)
-        //{
-        //    m_Velocity = m_Velocity.normalized * m_TerminalSpeed;
-        //}
+        if (m_Velocity.magnitude > 0)
+        {
+            m_DragForce = CalculateDrag(m_Velocity) * Time.deltaTime;
+
+            if (m_Velocity.magnitude <= m_DragForce.magnitude)
+            {
+                m_Velocity = Vector2.zero;
+            }
+            else
+            {
+                m_Velocity += m_DragForce;
+            }
+        }
+        else
+        {
+            m_DragForce = Vector2.zero;
+        }
+
         if (m_Velocity.magnitude < 0.01f)
         {
             m_Velocity = Vector2.zero;
         }
         #endregion
         #region 이동
-        transform.localPosition += (Vector3)m_Velocity * Time.deltaTime;
+        if (!m_IsKinematic)
+        {
+            transform.localPosition += (Vector3)m_Velocity * Time.deltaTime;
+        }
+        else
+        {
+            transform.localPosition += (Vector3)m_Force * Time.deltaTime;
+        }
         #endregion
         #region 초기화
-        m_Speed = m_Velocity.magnitude;
-        m_DragSpeed = m_DragForce.magnitude;
-        //m_TerminalSpeed = Mathf.Sqrt(Mathf.Abs((m_Mass * m_Gravity) / (m_Drag * 0.5f)));
+        if (!m_IsKinematic)
+        {
+            m_Speed = m_Velocity.magnitude;
+        }
+        else
+        {
+            m_Velocity = Vector2.zero;
+            m_Speed = m_Force.magnitude;
+        }
 
+        m_DragSpeed = m_DragForce.magnitude;
         m_Force = Vector2.zero;
         #endregion
     }
