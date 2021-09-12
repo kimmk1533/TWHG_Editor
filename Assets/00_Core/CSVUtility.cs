6fe0c40eb198ffa24c3e9b8abfd092e592a5dff5
kimmk1533 <55845465+kimmk1533@users.noreply.github.com>
2021-09-12 17:40:09 +0900

    CSV, XML Utility Update
    CSV(타일 저장용) 읽기쓰기 업데이트
    XML(오브젝트 저장용)은 업데이트 예정

---
+++ Assets/00_Core/CSVUtility.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+// https://bravenewmethod.com/2014/09/13/lightweight-csv-reader-for-unity/#comment-7111
+public static class CSVUtility
+{
+    static string SPLIT_RE = @",(?=(?:[^""]*""[^""]*"")*(?![^""]*""))";
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+    static char[] TRIM_CHARS = { '\"' };
+
+    public static List<Dictionary<string, object>> Read(string file, FileFormat format = FileFormat.csv)
+    {
+        string path = Path.Combine(Application.dataPath, file) + "." + format.ToString();
+        string data = new StreamReader(path).ReadToEnd();
+
+        string[] lines = Regex.Split(data, LINE_SPLIT_RE);
+
+        List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
+        if (lines.Length <= 1)
+            return list;
+
+        string[] header = Regex.Split(lines[0], SPLIT_RE);
+
+        for (int i = 1; i < lines.Length; ++i)
+        {
+            string[] values = Regex.Split(lines[i], SPLIT_RE);
+
+            if (values.Length == 0 || values[0] == "")
+                continue;
+
+            var entry = new Dictionary<string, object>();
+            for (int j = 0; j < header.Length && j < values.Length; ++j)
+            {
+                string value = values[j];
+                value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
+                object finalvalue = value;
+                int n;
+                float f;
+
+                if (int.TryParse(value, out n))
+                {
+                    finalvalue = n;
+                }
+                else if (float.TryParse(value, out f))
+                {
+                    finalvalue = f;
+                }
+                entry[header[j]] = finalvalue;
+            }
+
+            list.Add(entry);
+        }
+
+        return list;
+    }
+    public static void Write(string file, in List<Dictionary<string, object>> data)
+    {
+        Write(file, FileFormat.csv, data);
+    }
+    public static void Write(string file, FileFormat format, in List<Dictionary<string, object>> data)
+    {
+        string path = Path.Combine(Application.dataPath, file) + "." + format.ToString();
+        StreamWriter writer = new StreamWriter(path);
+
+        string key;
+        object item;
+
+        var enumerator = data[0].Keys.GetEnumerator();
+
+        #region ≈∞ ¿˙¿Â
+        for (int i = 0; i < data[0].Count - 1; ++i)
+        {
+            enumerator.MoveNext();
+            key = enumerator.Current;
+
+            writer.Write(key);
+            writer.Write(',');
+        }
+        enumerator.MoveNext();
+        key = enumerator.Current;
+        writer.WriteLine(key);
+        #endregion
+
+        for (int i = 0; i < data.Count; ++i)
+        {
+            enumerator = data[i].Keys.GetEnumerator();
+
+            for (int j = 0; j < data[i].Count - 1; ++j)
+            {
+                enumerator.MoveNext();
+                key = enumerator.Current;
+                item = data[i][key];
+
+                writer.Write(item);
+                writer.Write(',');
+            }
+
+            enumerator.MoveNext();
+            key = enumerator.Current;
+            item = data[i][key];
+
+            writer.WriteLine(item);
+        }
+
+        writer.Close();
+    }
+
+    public enum FileFormat
+    {
+        csv,
+        txt,
+    }
+}
---
+++ Assets/00_Core/XMLUtility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+using UnityEngine;
+
+public static class XMLUtility
+{
+
+}
